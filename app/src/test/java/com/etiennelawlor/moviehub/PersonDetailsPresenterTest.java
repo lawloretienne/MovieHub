c04416da5a2cca47d5f8291a78ba4aeb18877549
Etienne Lawlor <lawloretienne@gmail.com>
2017-03-10 10:38:48 -0800

    Added unit test for person details feature

---
+++ app/src/test/java/com/etiennelawlor/moviehub/PersonDetailsPresenterTest.java
@@ -0,0 +1,169 @@
+package com.etiennelawlor.moviehub;
+
+import com.etiennelawlor.moviehub.data.model.PersonDetailsWrapper;
+import com.etiennelawlor.moviehub.data.remote.response.Movie;
+import com.etiennelawlor.moviehub.data.remote.response.Person;
+import com.etiennelawlor.moviehub.data.remote.response.PersonCredit;
+import com.etiennelawlor.moviehub.data.remote.response.TelevisionShow;
+import com.etiennelawlor.moviehub.data.source.persondetails.PersonDetailsDataSourceContract;
+import com.etiennelawlor.moviehub.ui.persondetails.PersonDetailsPresenter;
+import com.etiennelawlor.moviehub.ui.persondetails.PersonDetailsUiContract;
+import com.etiennelawlor.moviehub.util.rxjava.TestSchedulerTransformer;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.Observable;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by etiennelawlor on 2/9/17.
+ */
+
+public class PersonDetailsPresenterTest {
+
+    // region Test Doubles
+
+    // Mocks
+    @Mock
+    private PersonDetailsUiContract.View mockPersonDetailsView;
+    @Mock
+    private PersonDetailsDataSourceContract.Repository mockPersonDetailsRepository;
+
+    // Stubs
+    private Observable stub;
+
+    // endregion
+
+    // region Member Variables
+    private PersonDetailsWrapper personDetailsWrapper;
+    private PersonDetailsPresenter personDetailsPresenter;
+    // endregion
+
+    @Before
+    public void setUp() {
+        // Mockito has a very convenient way to inject mocks by using the @Mock annotation. To
+        // inject the mocks in the test the initMocks method needs to be called.
+        MockitoAnnotations.initMocks(this);
+
+        // Get a reference to the class under test
+        personDetailsPresenter = new PersonDetailsPresenter(mockPersonDetailsView, mockPersonDetailsRepository, new TestSchedulerTransformer<PersonDetailsWrapper>());
+    }
+
+    // region Test Methods
+//    @Test(expected = IOException.class)
+    @Test
+    public void onLoadTelevisionShowDetails_shouldShowError_whenRequestFailed() {
+        // 1. (Given) Set up conditions required for the test
+        Person person = new Person();
+        person.setId(1);
+        List<PersonCredit> cast = new ArrayList<>();
+        List<PersonCredit> crew = new ArrayList<>();
+        personDetailsWrapper = new PersonDetailsWrapper(person, cast, crew);
+        stub = Observable.<PersonDetailsWrapper>error(new IOException());
+        when(mockPersonDetailsRepository.getPersonDetails(anyInt())).thenReturn(stub);
+
+        // 2. (When) Then perform one or more actions
+        personDetailsPresenter.onLoadPersonDetails(person.getId());
+
+        // 3. (Then) Afterwards, verify that the state you are expecting is actually achieved
+        verify(mockPersonDetailsView).showErrorView();
+    }
+
+    @Test
+    public void onLoadTelevisionShowDetails_shouldShowTelevisionShowDetails_whenRequestSucceeded() {
+        // 1. (Given) Set up conditions required for the test
+        Person person = new Person();
+        person.setId(1);
+        List<PersonCredit> cast = new ArrayList<>();
+        List<PersonCredit> crew = new ArrayList<>();
+        personDetailsWrapper = new PersonDetailsWrapper(person, cast, crew);
+        stub = Observable.just(personDetailsWrapper);
+        when(mockPersonDetailsRepository.getPersonDetails(anyInt())).thenReturn(stub);
+
+        // 2. (When) Then perform one or more actions
+        personDetailsPresenter.onLoadPersonDetails(person.getId());
+
+        // 3. (Then) Afterwards, verify that the state you are expecting is actually achieved
+        verify(mockPersonDetailsView).showPersonDetails(personDetailsWrapper);
+    }
+
+    @Test
+    public void onMovieClick_shouldOpenMovieDetails() {
+        // 1. (Given) Set up conditions required for the test
+        Movie movie = new Movie();
+
+        // 2. (When) Then perform one or more actions
+        personDetailsPresenter.onMovieClick(movie);
+
+        // 3. (Then) Afterwards, verify that the state you are expecting is actually achieved
+        verify(mockPersonDetailsView).openMovieDetails(movie);
+
+        verifyZeroInteractions(mockPersonDetailsRepository);
+    }
+
+    @Test
+    public void onTelevisionShowClick_shouldOpenTelevisionShowDetails() {
+        // 1. (Given) Set up conditions required for the test
+        TelevisionShow televisionShow = new TelevisionShow();
+
+        // 2. (When) Then perform one or more actions
+        personDetailsPresenter.onTelevisionShowClick(televisionShow);
+
+        // 3. (Then) Afterwards, verify that the state you are expecting is actually achieved
+        verify(mockPersonDetailsView).openTelevisionShowDetails(televisionShow);
+
+        verifyZeroInteractions(mockPersonDetailsRepository);
+    }
+
+    @Test
+    public void onScrollChange_shouldShowToolbarTitle_whenScrolledPastThreshold() {
+        // 1. (Given) Set up conditions required for the test
+        boolean isScrolledPastThreshold = true;
+
+        // 2. (When) Then perform one or more actions
+        personDetailsPresenter.onScrollChange(isScrolledPastThreshold);
+
+        // 3. (Then) Afterwards, verify that the state you are expecting is actually achieved
+        verify(mockPersonDetailsView).showToolbarTitle();
+
+        verifyZeroInteractions(mockPersonDetailsRepository);
+    }
+
+    @Test
+    public void onScrollChange_shouldHideToolbarTitle_whenNotScrolledPastThreshold() {
+        // 1. (Given) Set up conditions required for the test
+        boolean isScrolledPastThreshold = false;
+
+        // 2. (When) Then perform one or more actions
+        personDetailsPresenter.onScrollChange(isScrolledPastThreshold);
+
+        // 3. (Then) Afterwards, verify that the state you are expecting is actually achieved
+        verify(mockPersonDetailsView).hideToolbarTitle();
+
+        verifyZeroInteractions(mockPersonDetailsRepository);
+    }
+
+    @Test
+    public void onDestroyView_shouldNotHaveInteractions() {
+        // 1. (Given) Set up conditions required for the test
+
+        // 2. (When) Then perform one or more actions
+        personDetailsPresenter.onDestroyView();
+        // 3. (Then) Afterwards, verify that the state you are expecting is actually achieved
+        verifyZeroInteractions(mockPersonDetailsView);
+        verifyZeroInteractions(mockPersonDetailsRepository);
+    }
+
+    // endregion
+}
