98e034bf80fad9acbc8af1da9053d6519beedfca
Etienne Lawlor <lawloretienne@gmail.com>
2017-12-22 13:40:31 -0800

    Removed rxjava1 dependencies

--- app/src/main/java/com/etiennelawlor/moviehub/data/network/ServiceGenerator.java
+++ app/src/main/java/com/etiennelawlor/moviehub/data/network/ServiceGenerator.java
@@ -10,7 +10,6 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import timber.log.Timber;
@@ -26,7 +25,6 @@ public class ServiceGenerator {
 
     private static Retrofit.Builder retrofitBuilder
             = new Retrofit.Builder()
-            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create());
 
